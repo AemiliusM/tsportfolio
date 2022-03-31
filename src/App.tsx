420398937bd2a63b54cfa9ce0a7e11b6120d4d56
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-03-30 17:04:46 -0700

    added more detail and patterns to left screen and keypad

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import { brewletteItems } from './imageSrc';
 import Slider from './Project/Slider';
 
 function App() {
+  const descriptionText1 =
+    'Takes the hassle out of picking a place to go for drinks';
+  const descriptionText2 =
+    'Created using React.js for front-end and SQL and Node.js for the back-end';
+  const descriptionText3 = 'Mobile Forward App';
   return (
     <div className='App'>
       <div className='console-box'>
@@ -14,13 +19,35 @@ function App() {
           </div>
           <div className='background-shaper'></div>
           <div className='black-text-container'>
-            <h3 className='title-text'>Brewlette</h3>
-            <div className='section-back-light'>
-              {'   '}
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              </span>
-            </div>
+            <h3 className='title-text'>BREWLETTE</h3>
+            <div className='section-back-light'>{'   '}</div>
+            <ul className='description-text-list'>
+              {descriptionText1.split(' ').map((text) => {
+                return (
+                  <li className='description-text' key={text}>
+                    {text}
+                  </li>
+                );
+              })}
+            </ul>
+            <ul className='description-text-list'>
+              {descriptionText2.split(' ').map((text1) => {
+                return (
+                  <li className='description-text' key={text1}>
+                    {text1}
+                  </li>
+                );
+              })}
+            </ul>
+            <ul className='description-text-list'>
+              {descriptionText3.split(' ').map((text2) => {
+                return (
+                  <li className='description-text' key={text2}>
+                    {text2}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
         <div className='console-divider-box-left'></div>
@@ -29,7 +56,43 @@ function App() {
         <div className='console-screen'></div>
       </div>
       <div className='console-interface-box'>
-        <div className='console-interface-key-pad'></div>
+        <div className='console-interface-key-pad'>
+          <div className='key-pad-box'></div>
+          <div className='key-pad-box-2'>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <a
+              className='brewlette-git-hub'
+              href='https://github.com/BREWLETTE'
+            >
+              Code
+            </a>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <a className='brewlette-site' href='https://www.brewlette.rocks/'>
+              Site
+            </a>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+            <div className='key-pad-button'></div>
+          </div>
+          <div className='key-pad-box-3'></div>
+        </div>
         <div className='console-interface-divider-box-left'></div>
         <div className='console-interface-key-pad'></div>
         <div className='console-interface-divider-box-right'></div>
