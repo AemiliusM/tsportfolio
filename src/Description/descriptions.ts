70f337b3f6e9d237ef56830732966698c365c464
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-04-19 12:18:04 -0700

    adding rem instead of px

--- src/Description/descriptions.ts
+++ src/Description/descriptions.ts
@@ -2,11 +2,12 @@ export const descriptionText1 =
   'Takes the hassle out of picking a place to go for drinks';
 export const descriptionText2 =
   'Created using React.js for front-end and SQL and Node.js for the back-end';
-export const descriptionText3 = 'Mobile Forward App';
+export const descriptionText3 =
+  'Mobile designed app to for quick and easy use while out on the town';
 
 export const descriptionText4 =
   "Quirky site that gives statistics based off a user's birthday";
 export const descriptionText5 =
-  'Created using React.js for front end, supabase for OAuth, and Express for back-end';
+  'Created using React.js for front-end, supabase for OAuth, and Express for back-end';
 export const descriptionText6 =
   'Most interesting for me was the calculations for each statistic';
